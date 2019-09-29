c26979a625a474f95b2cd6e6c3596d90869febe7
Mathieu Benoit <mathieu.benoit@mathben.tech>
2019-09-29 19:03:08 -0400

    [IMP] CRM: Increase priority 3 to 5 stars

--- addons/crm/models/crm_stage.py
+++ addons/crm/models/crm_stage.py
@@ -4,10 +4,12 @@
 from odoo import api, fields, models
 
 AVAILABLE_PRIORITIES = [
-    ('0', 'Low'),
-    ('1', 'Medium'),
-    ('2', 'High'),
-    ('3', 'Very High'),
+    ('0', 'None'),
+    ('1', 'Poor'),
+    ('2', 'Low'),
+    ('3', 'Medium'),
+    ('4', 'High'),
+    ('5', 'Very High'),
 ]
 
 
